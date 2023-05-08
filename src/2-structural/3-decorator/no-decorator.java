1ea3bedfde8684cada5e5ef6ac6e3e5341f2e7d5
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-08 16:38:24 +0200

    feat: remove java translations

--- src/2-structural/3-decorator/no-decorator.java
+++
@@ -1,9 +0,0 @@
-// ! ❌ Bad example not using decorator
-public class Enrolment {
-  public String enrol(String activityId, String customerId, int numPlaces) {
-    String enrollment = String.format("Enrolled %s in %s with %d places", customerId, activityId, numPlaces);
-    System.out.println(enrollment);
-    return enrollment;
-  }
-  // ToDo: 😱 add cancellation new functionality
-}
