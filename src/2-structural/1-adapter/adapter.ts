66796aa4ed7802fdfd59e0a23ec3c35654ec6025
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-11 11:48:17 +0200

    fix: adapter

--- src/2-structural/1-adapter/adapter.ts
+++ src/2-structural/1-adapter/adapter.ts
@@ -1,23 +1,29 @@
 import { ExternalEventData, ExternalEventService } from "./external-event.library";
-import { LogEntry } from "./logger.application";
 
 // * ✅ Adapter solution
-
 // * Define your desired Interface (or use an existing one)
+
+export type LogCategory = "info" | "error" | "debug";
+export type LogEntry = {
+  category: LogCategory;
+  message: string;
+  timestamp: Date;
+};
 export interface Logger {
   log(entry: LogEntry): string;
 }
-// * make an adpater implementing the desired interface
-export class CommonEventAdapter implements Logger {
+
+// * make an adapter implementing the desired interface
+export class ExternalEventServiceAdapter implements Logger {
   // * 😏 The adapted class is wrapped in a private property
-  private commonEventService: ExternalEventService = new ExternalEventService();
+  private externalEventService: ExternalEventService = new ExternalEventService();
   // * 😏 The rest of the world only sees the desired interface
   log(entry: LogEntry): string {
     // * 😏 knowledge of the proprietary workflow is encapsulated in the adapter
     const commonEvent = this.adaptLogEntryToExternalEvent(entry);
-    const commonEventMessage = this.commonEventService.createMessage(commonEvent);
+    const commonEventMessage = this.externalEventService.createMessage(commonEvent);
     // Todo: change the writer or make it configurable
-    return this.commonEventService.writeMessage(commonEventMessage);
+    return this.externalEventService.writeMessage(commonEventMessage);
   }
   // * 😏 all the ugly stuff is hidden in the adapter
   private adaptLogEntryToExternalEvent(entry: LogEntry): ExternalEventData {
@@ -30,13 +36,17 @@ export class CommonEventAdapter implements Logger {
     };
   }
 }
+function loggerFactory(): Logger {
+  // * 😏 can use any other adaptation
+  return new ExternalEventServiceAdapter();
+}
 
 export class Client {
   // * 😏 client classes are decoupled from the concrete implementation
   private readonly logger: Logger;
   constructor() {
-    // * 😏 Could come from a factory
-    this.logger = new CommonEventAdapter();
+    // * 😏 comes from a factory, don't care about the implementation
+    this.logger = loggerFactory();
   }
   public doThings() {
     // * 😏 client classes are decoupled from the interface
--- src/2-structural/1-adapter/logger.application.ts
+++
@@ -1,7 +0,0 @@
-export type LogCategory = "info" | "error" | "debug";
-
-export type LogEntry = {
-  category: LogCategory;
-  message: string;
-  timestamp: Date;
-};
