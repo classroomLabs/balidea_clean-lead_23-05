adb66e526e9f141b827040230fc4bec1c9589de1
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-29 19:10:36 +0200

    feat: no command

--- src/3-behavioral/3-command/no-command.ts
+++ src/3-behavioral/3-command/no-command.ts
@@ -1,5 +1,21 @@
 // ! ❌ Bad example not using a command
 
+// The invoker
+export class EnrolmentController {
+  private service: EnrolmentService = new EnrolmentService();
+  private paymentService: PaymentService = new PaymentService();
+  // ! 😱 tight coupling invoker and receiver
+  enroll(activity: string, participant: string): void {
+    //console.log(`Enrolling ${participant} in ${activity}`);
+    this.service.enroll(activity, participant);
+    this.paymentService.pay(activity, participant);
+  }
+  unEnrollment(activity: string, participant: string): void {
+    this.service.unenroll(activity, participant);
+    this.paymentService.refund(activity, participant);
+  }
+}
+
 // The receiver
 export class EnrolmentService {
   enroll(activity: string, participant: string): void {
@@ -11,15 +27,72 @@ export class EnrolmentService {
   }
 }
 
-// The invoker
-export class EnrolmentController {
-  private service: EnrolmentService = new EnrolmentService();
-
-  // ! 😱 tight coupling invoker and receiver
-  dispatchEnrollment(activity: string, participant: string): void {
-    this.service.enroll(activity, participant);
+export class PaymentService {
+  pay(activity: string, participant: string): void {
+    console.log(`Paying ${participant} for ${activity}`);
   }
-  dispatchUnEnrollment(activity: string, participant: string): void {
+  refund(activity: string, participant: string): void {
+    console.log(`Refunding ${participant} for ${activity}`);
+  }
+}
+
+export interface Command {
+  command: string;
+  execute(): void;
+}
+
+export class EnrolmentFacade {
+  private service: EnrolmentService = new EnrolmentService();
+  private paymentService: PaymentService = new PaymentService();
+  enroll(activity: string, participant: string): void {
+    this.service.enroll(activity, participant);
+    this.paymentService.pay(activity, participant);
+  }
+  unenroll(activity: string, participant: string): void {
     this.service.unenroll(activity, participant);
+    this.paymentService.refund(activity, participant);
+  }
+}
+
+export class EnrollCommand implements Command {
+  public command = "Enroll";
+  private receiver: EnrolmentFacade = new EnrolmentFacade();
+
+  constructor(public data: { participant: string; activity: string }) {}
+
+  execute(): void {
+    this.receiver.enroll(this.data.activity, this.data.participant);
+    //console.log(`Enrolling ${this.data.participant} in ${this.data.activity}`);
+  }
+}
+
+export class UnEnrollCommand implements Command {
+  public command = "UnEnroll";
+  private receiver: EnrolmentFacade = new EnrolmentFacade();
+  constructor(public data: { participant: string; activity: string }) {}
+  execute(): void {
+    this.receiver.unenroll(this.data.activity, this.data.participant);
+  }
+  undo(): void {
+    this.receiver.enroll(this.data.activity, this.data.participant);
+  }
+  redo(): void {
+    this.execute();
+  }
+}
+
+interface CommandState {
+  previousState: any;
+  currentState: any;
+  command: Command;
+}
+
+export class CommandProcessor {
+  history: Command[] = [];
+  dispatch(command: Command): void {
+    this.history.push(command);
+    console.log("CommandProcessor: Dispatching command", command);
+    command.execute();
+    console.log("CommandProcessor: Command dispatched", command);
   }
 }
